c94853baa0334219aba4c6f7c7af4400fb5ae5d0
Trukhym Viktoriia <vtruhym@gmail.com>
2026-05-09 19:58:42 +0300

    Connected cloud database

--- server.js
+++ server.js
@@ -19,9 +19,14 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // MONGO
-mongoose.connect("mongodb://127.0.0.1:27017/qrfound")
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.log(err));
+// Шукаємо змінну від Render, якщо її немає — беремо локальну базу
+const mongoose = require('mongoose');
+
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/qrfound';
+
+mongoose.connect(dbURI)
+  .then(() => console.log('✅ Підключено до хмарної бази MongoDB!'))
+  .catch(err => console.error('❌ Помилка підключення:', err));
 
 // ROUTES
 const routes = require('./app');
